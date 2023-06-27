ae9832898f5aed4e455d5fbdc5f478e7df70adbe
bav0363 <129315926+bav0363@users.noreply.github.com>
2023-06-27 21:18:00 +0300

    Add files via upload

--- Задача№7.py
+++ Задача№7.py
@@ -1,6 +1,6 @@
 year = int(input("Введите год: "))
 if (year % 4 == 0 or year % 400 == 0) and year % 100 != 0:
-    print("YES")
+    print("Год является високосным!")
 else:
-    print("NO")
+    print("Год не является високосным")
 
