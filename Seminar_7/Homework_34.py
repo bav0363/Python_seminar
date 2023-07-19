017a4a70f702bf61ee000b1618a5da8938a6a568
bav0363 <129315926+bav0363@users.noreply.github.com>
2023-07-19 21:39:52 +0300

    Add files via upload

--- Seminar_7/Homework_34.py
+++ Seminar_7/Homework_34.py
@@ -1,38 +1,25 @@
+# Функция для подсчета гласных в каждом слове
+def vowels_in_word(my_list):
+    for i in poem:
+        count = 0
+        for j in i:
+            if j in vowels:
+                count += 1
+        number_vowels.append(count)
+    return number_vowels
 
-vowels = 'ауоыэяюёие'
-print(vowels)
 
-list_vowels = [i for i in vowels]
-print(list_vowels)
-
-poem = 'пара-ра-рам рам-пам-папам па-ра-па-дам'
-list_poem = poem.split()
-print(list_poem)
-
-number_vowels = []
-for i in list_poem:
-    count = 0
-    for j in i:
-        if j in list_vowels:
-            count += 1
-    number_vowels.append(count)
-print(number_vowels)
-
+# Функция для проверки правила рифмы
 def result(my_list):
     for i in range(1, len(my_list)):
         if my_list[0] != my_list[i]:
             return 'Пам парам'
     return 'Парам пам-пам'
 
+
+vowels = 'ауоыэяюёие'  # Все гласные в алфавите
+poem = 'пара-ра-рам рам-пам-папам па-ра-па-дам'  # Строка для проверки на рифму
+list_poem = poem.split()
+number_vowels = []
+vowels_in_word(number_vowels)
 print(result(number_vowels))
-
-
-
-
-
-
-
-
-
-
-
